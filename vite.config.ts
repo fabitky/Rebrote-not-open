ce7d095b8da61062538024617b1b672beaea3865
fabitky <fabitky@gmail.com>
2026-01-22 15:45:55 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json']
-  },
   build: {
     outDir: 'dist',
     target: 'esnext',
-    minify: 'esbuild'
+    emptyOutDir: true
+  },
+  server: {
+    port: 3000
   }
 });
